09bcb5c471cc6cae0569426c3b10153eff1f8b41
Martin <martinsanchez2533@gmail.com>
2024-06-20 10:40:16 -0300

    estructura del proyecto

---
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+})
+export class RegisterPageComponent { }
---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-page',
+  templateUrl: './basic-page.component.html',
+  styles: ``
+})
+export class BasicPageComponent {
+
+  // public myForm: FormGroup= new FormGroup({
+  //   name: new FormControl("",),
+  //   price: new FormControl("", ),
+  //   inStorage: new FormControl("", ),
+  // })
+
+  public myForm: FormGroup = this.fb.group({
+    name: [""],
+    price: [0],
+    inStorage: [0]
+  })
+
+  constructor(private fb: FormBuilder) { }
+
+  onSave(): void {
+    console.log(this.myForm.value)
+  }
+
+}
