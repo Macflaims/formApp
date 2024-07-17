4e16a62898dd72160ae2ac0fceec78ee346bfa0b
Martin <martinsanchez2533@gmail.com>
2024-07-17 08:49:35 -0300

    agrega validaciones asíncronas

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from '../../../shared/service/validators.service';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styles: `
-    :host {
-      display: block;
-    }
-  `,
 })
-export class RegisterPageComponent { }
+export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ["", [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)]],
+    email: ["", [Validators.required, Validators.pattern(this.validatorsService.emailPattern)]],
+    username: ["", [Validators.required, this.validatorsService.cantBeStrider]],
+    password: ["", [Validators.required, Validators.minLength(6)]],
+    password2: ["", [Validators.required]],
+  }, {
+    validators: [
+      this.validatorsService.isFieldoneEqualFieldTwo("password", "password2"),
+    ]
+  })
+
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService) { }
+
+  isInvalidField(field: string) {
+    return this.validatorsService.isInValidField(this.myForm, field)
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched()
+    console.log(this.myForm.value)
+  }
+
+}
